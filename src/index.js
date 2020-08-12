fb0c497a59563d7e341c64a27a0bc8169d528361
MFAC <ninafroes@gmail.com>
2020-08-12 16:11:59 +0200

    feat: Add an entry point

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppointmentsDayView } from './Appointment';
+import { sampleAppointments } from './sampleData';
+
+ReactDOM.render(
+  <AppointmentsDayView
+    appointments={sampleAppointments}
+  />,
+  document.getElementById('root')
+);

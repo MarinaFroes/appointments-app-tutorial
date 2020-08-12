3444b43016d14b36318d0f812296d3505b982601
MFAC <ninafroes@gmail.com>
2020-08-12 14:55:07 +0200

    test: Check if there's a message within container when there's no appointments for the day

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -62,5 +62,10 @@ describe('AppointmentsDayView', () => {
     expect(container.querySelectorAll('li')).toHaveLength(2);
     expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
     expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
-  })
+  });
+
+  it('initially shows a message saying there are no appointments today', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch('There are no appointments scheduled for today.');
+  });
 });

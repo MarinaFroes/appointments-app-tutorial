27bbcc59ad83ce80bac9790f1a3be6e3f00d8e90
MFAC <ninafroes@gmail.com>
2020-08-17 15:31:02 +0200

    refactor: Add a function to select the form with the id passed as argument

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -8,12 +8,13 @@ describe('CustomerForm', () => {
   beforeEach(() => {
     ({ render, container } = createContainer());
   });
+
+  const form = id => container.querySelector(`form[id="${id}"]`);
   
   it('renders a form', () => {
     render(<CustomerForm />);
 
-    expect(
-      container.querySelector('form[id="customer"]')
-    ).not.toBeNull();
+    expect(form("customer")).not.toBeNull();
   });
+
 });

444c73b124bfb1864d3d3b2dc5d45d01e8ff761e
MFAC <ninafroes@gmail.com>
2020-08-17 15:54:39 +0200

    refactor: Extract firstNameField() function

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -16,6 +16,8 @@ describe('CustomerForm', () => {
     expect(formElement.tagName).toEqual('INPUT');
     expect(formElement.type).toEqual('text');
   };
+
+  const firstNameField = () => form('customer').elements.firstName;
   
   it('renders a form', () => {
     render(<CustomerForm />);
@@ -26,13 +28,13 @@ describe('CustomerForm', () => {
   it('renders the first name field as a text box', () => {
     render(<CustomerForm />);
 
-    const field = form('customer').elements.firstName;
+    const field = firstNameField();
     expectToBeInputFieldOfTypeText(field);
   });
 
   it('includes the existing value for the first name', () => {
     render(<CustomerForm firstName="Ashley" />);
-    const field = form('customer').elements.firstName;
+    const field = firstNameField();
     expect(field.value).toEqual('Ashley');
   });
 });
